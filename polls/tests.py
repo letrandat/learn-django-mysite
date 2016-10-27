75c55f4690685f15bb00f1e48ff21c91c1d7e163
Le Tran Dat <trandatnh@gmail.com>
2016-10-27 10:59:34 +0800

    Optimize Imports

--- polls/tests.py
+++ polls/tests.py
@@ -1,8 +1,8 @@
 import datetime
 
-from django.utils import timezone
-from django.urls import reverse
 from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
 
 from .models import Question
 
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render
-
 # Create your views here.
-from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.views import generic
 from django.utils import timezone
-
+from django.views import generic
 
 from .models import Choice, Question
 
